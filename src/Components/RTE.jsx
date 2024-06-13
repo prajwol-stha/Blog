b22174e2977fe2d86f9e221beabcadaf309c5135
Prajwol Shrestha <prajwol.stha777@gmail.com>
2024-06-13 09:37:24 +0545

    deploy

--- src/Components/RTE.jsx
+++ src/Components/RTE.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Editor } from '@tinymce/tinymce-react';
 import {Controller } from 'react-hook-form';
-import {tinyMceApi} from '../apiKeys'
 
 
 export default function RTE({name, control, label, defaultValue =""}) {
@@ -15,7 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey={tinyMceApi}
+        apiKey={import.meta.env.VITE_TINYMCE_API}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
